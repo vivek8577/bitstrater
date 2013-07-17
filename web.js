2f26369c43402b6bf5c5c4129552d2f80eeaa6e6
vivek8577 <garg_v@yahoo.com>
2013-07-17 03:46:34 +0000

    web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs = require('fs');
-var buffer = new Buffer (fs.readFileSync("index.html", "utf-8")); 
+var buf = new Buffer (fs.readFileSync("index.html", "utf-8")); 
  response.send(buf.toString());
 });
 
